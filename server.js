72b4f6e65a67f5be67230817359c4fd4589b8fb6
Ben Kaan <benkaan001@gmail.com>
2021-11-27 02:18:27 -0600

    set up routes and create POST API on the client side

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+// require("dotenv").config();
+// const connectDB = require("./db/connect");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
+
+// const port = process.env.PORT || 3001;
+// const start = async () => {
+//   try {
+//     await connectDB(process.env.MONGO_URI);
+//     app.listen(port, () => {
+//       console.log(`=> 🌍 => server is listening on PORT ${port}......`);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// start();
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
